e7f7f7d0f47bef8ce5eed797cab25fc134008aaa
maddclif <maddclif24@gmail.com>
2022-02-06 18:38:06 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: './src/init.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
