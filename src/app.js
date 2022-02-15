68c3a47ed8210c6310e86cd2f6df3670d2c9c98a
maddclif <maddclif24@gmail.com>
2022-02-15 18:16:37 +0300

    Fix errors 3

--- src/app.js
+++ src/app.js
@@ -25,6 +25,10 @@ const app = (state, watchState, i18next) => {
   });
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    if (state.form.rssLinks.includes(state.form.inputUrl)) {
+      watchState.form.error = i18next.t('duplicateUrl');
+      return;
+    }
     schema
       .validate({
         url: state.form.inputUrl,
@@ -33,10 +37,6 @@ const app = (state, watchState, i18next) => {
         axios.get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(`${data.url}`)}&disableCache=true)`)
           .then((res) => {
             const parse = parserDom(res.data.contents);
-            if (state.form.rssLinks.includes(state.form.inputUrl)) {
-              watchState.form.error = i18next.t('duplicateUrl');
-              return;
-            }
             watchState.validateForm = 'is-valid';
             watchState.form.posts = generatedId([...parse.postsParse, ...state.form.posts]);
             watchState.form.feeds = [parse.feedParse, ...state.form.feeds];
