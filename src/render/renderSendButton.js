052550879bdc370116105b728f61beb188f211fe
maddclif <maddclif24@gmail.com>
2022-02-17 19:34:48 +0300

    Fix button

--- src/render/renderSendButton.js
+++ src/render/renderSendButton.js
@@ -1,6 +1,6 @@
 export default (value) => {
   const button = document.querySelector('[aria-label="add"]');
   if (value) {
-    button.setAttribute('disabled', 'disabled');
-  } else button.removeAttribute('disabled');
+    button.setAttribute('readonly', 'readonly');
+  } else button.removeAttribute('readonly');
 };
