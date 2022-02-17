d1e1cbdaa8e9a2817a4e2440aaaae99ae8c75094
maddclif <maddclif24@gmail.com>
2022-02-17 19:40:16 +0300

    Fix button 3

--- src/render/renderSendButton.js
+++ src/render/renderSendButton.js
@@ -1,6 +1,6 @@
 export default (value) => {
   const button = document.querySelector('[aria-label="add"]');
   if (value) {
-    button.setAttribute('readonly');
+    button.setAttribute('readonly', true);
   } else button.removeAttribute('readonly');
 };
